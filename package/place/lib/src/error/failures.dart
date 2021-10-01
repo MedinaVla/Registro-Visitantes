47d3820ba35293435e0f3d1473fba45c158f10f8
MedinaVla <medinavladimir14@gmail.com>
2021-10-01 16:05:12 -0400

    Fixing errors

--- package/place/lib/src/error/failures.dart
+++ package/place/lib/src/error/failures.dart
@@ -1,1 +1,10 @@
-import 'package:equatable/equatable.dart';
+// import 'package:equatable/equatable.dart';
+
+// abstract class Failure extends Equatable {
+//   @override
+//   List<Object> get props => [];
+// }
+
+// class ServerFailure extends Failure {}
+
+// class CacheFailure extends Failure {}
