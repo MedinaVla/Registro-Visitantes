599410423574c86f300806aad5818749fcadf0a4
MedinaVla <medinavladimir14@gmail.com>
2021-10-06 10:30:30 -0400

    Update Theme PaginatedDataTable

--- lib/src/features/listar/views/widgets/show_visitors.dart
+++ lib/src/features/listar/views/widgets/show_visitors.dart
@@ -1,3 +1,95 @@
+// import 'package:admin/src/core/colors.dart';
+// import 'package:admin/src/core/styles.dart';
+// import 'package:admin/src/features/listar/visitor/logic/visitor_provider.dart';
+// import 'package:data_table_2/data_table_2.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter_riverpod/flutter_riverpod.dart';
+// import 'package:flutter_svg/svg.dart';
+
+// import 'recent_file_model.dart';
+
+// class ShowVisitors extends ConsumerWidget {
+//   const ShowVisitors({
+//     Key? key,
+//   }) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context, ScopedReader watch) {
+//     final allVisitors = watch(visitorNotifierProvider);
+//     return Container(
+//       padding: EdgeInsets.all(defaultPadding),
+//       decoration: BoxDecoration(
+//         color: secondaryColor,
+//         borderRadius: const BorderRadius.all(Radius.circular(10)),
+//       ),
+//       child: Column(
+//         crossAxisAlignment: CrossAxisAlignment.start,
+//         children: [
+//           Text(
+//             "Recent Files",
+//             style: Theme.of(context).textTheme.subtitle1,
+//           ),
+//           SizedBox(
+//             width: double.infinity,
+//             child: allVisitors.when(
+//               initial: () => Text(''),
+//               data: (visitors) => dataTableVisitors(visitors),
+//               loading: () => Text(''),
+//               error: (error) => Text(
+//                 error.toString(),
+//               ),
+//             ),
+//           )
+//         ],
+//       ),
+//     );
+//   }
+// }
+
+// Widget dataTableVisitors(visitors) {
+//   return DataTable2(
+//     columnSpacing: defaultPadding,
+//     minWidth: 600,
+//     columns: [
+//       DataColumn(
+//         label: Text("Nombre"),
+//       ),
+//       DataColumn(
+//         label: Text("Apellidos"),
+//       ),
+//       DataColumn(
+//         label: Text("CI"),
+//       ),
+//       DataColumn(
+//         label: Text("Solapin"),
+//       ),
+//       DataColumn(
+//         label: Text("Area"),
+//       ),
+//       DataColumn(
+//         label: Text("Trabajador"),
+//       ),
+//     ],
+//     rows: List.generate(
+//       visitors.length,
+//       (index) => recentFileDataRow(visitors[index]),
+//     ),
+//   );
+// }
+
+// DataRow recentFileDataRow(Visitor visitor) {
+//   return DataRow(
+//     cells: [
+//       DataCell(Text(visitor.name)),
+//       DataCell(Text(visitor.spell)),
+//       DataCell(Text(visitor.ci.toString())),
+//       DataCell(Text(visitor.solapin.toString())),
+//       DataCell(Text(visitor.namePlace)),
+//       DataCell(Text(visitor.nameWorker)),
+//     ],
+//   );
+// }
+
 import 'package:admin/src/core/colors.dart';
 import 'package:admin/src/core/styles.dart';
 import 'package:admin/src/features/listar/visitor/logic/visitor_provider.dart';
@@ -9,41 +101,39 @@ import 'package:flutter_svg/svg.dart';
 import 'recent_file_model.dart';
 
 class ShowVisitors extends ConsumerWidget {
-  ShowVisitors({
+  const ShowVisitors({
     Key? key,
   }) : super(key: key);
-  final ScrollController _controller = ScrollController();
 
   @override
   Widget build(BuildContext context, ScopedReader watch) {
     final allVisitors = watch(visitorNotifierProvider);
-
     return Container(
       padding: EdgeInsets.all(defaultPadding),
-      decoration: BoxDecoration(
-        color: secondaryColor,
-        borderRadius: const BorderRadius.all(Radius.circular(10)),
-      ),
+      // decoration: BoxDecoration(
+      //   color: secondaryColor,
+      //   borderRadius: const BorderRadius.all(Radius.circular(10)),
+      // ),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text(
-            "Recent Files",
-            style: Theme.of(context).textTheme.subtitle1,
-          ),
+          // Text(
+          //   "Recent Files",
+          //   style: Theme.of(context).textTheme.subtitle1,
+          // ),
           SizedBox(
             width: double.infinity,
             child: allVisitors.when(
               initial: () => Text(''),
-              data: (visitors) => dataTableVisitors(visitors, _controller),
+              data: (visitors) => Theme(
+                data: Theme.of(context).copyWith(cardColor: secondaryColor),
+                child: dataTableVisitors(visitors),
+              ),
               loading: () => Text(''),
               error: (error) => Text(
                 error.toString(),
               ),
             ),
-          ),
-          _ScrollUpButton(
-            controller: _controller,
           )
         ],
       ),
@@ -51,100 +141,56 @@ class ShowVisitors extends ConsumerWidget {
   }
 }
 
-Widget dataTableVisitors(visitors, _controller) {
-  return DataTable2(
-    scrollController: _controller,
-    columnSpacing: defaultPadding,
-    minWidth: 600,
+Widget dataTableVisitors(visitors) {
+  DataTableSource _data = MyData(visitors: visitors);
+
+  return PaginatedDataTable(
+    source: _data,
+    header: Text('My Products'),
     columns: [
-      DataColumn(
-        label: Text("Nombre"),
-      ),
-      DataColumn(
-        label: Text("Apellidos"),
-      ),
-      DataColumn(
-        label: Text("CI"),
-      ),
-      DataColumn(
-        label: Text("Solapin"),
-      ),
-      DataColumn(
-        label: Text("Area"),
-      ),
-      DataColumn(
-        label: Text("Trabajador"),
-      ),
+      DataColumn(label: Text('Nombre')),
+      DataColumn(label: Text('Apellidos')),
+      DataColumn(label: Text('CI')),
+      DataColumn(label: Text('Solapin')),
+      DataColumn(label: Text('Area')),
+      DataColumn(label: Text('Trabajador')),
     ],
-    rows: List.generate(
-      visitors.length,
-      (index) => recentFileDataRow(visitors[index]),
-    ),
+    columnSpacing: 100,
+    horizontalMargin: 20,
+    rowsPerPage: 8,
+    showCheckboxColumn: false,
   );
 }
 
-DataRow recentFileDataRow(Visitor visitor) {
-  return DataRow(
-    cells: [
-      DataCell(Text(visitor.name)),
-      DataCell(Text(visitor.spell)),
-      DataCell(Text(visitor.ci.toString())),
-      DataCell(Text(visitor.solapin.toString())),
-      DataCell(Text(visitor.namePlace)),
-      DataCell(Text(visitor.nameWorker)),
-    ],
-  );
-}
+// DataRow recentFileDataRow(Visitor visitor) {
+//   return DataRow(
+//     cells: [
+//       DataCell(Text(visitor.name)),
+//       DataCell(Text(visitor.spell)),
+//       DataCell(Text(visitor.ci.toString())),
+//       DataCell(Text(visitor.solapin.toString())),
+//       DataCell(Text(visitor.namePlace)),
+//       DataCell(Text(visitor.nameWorker)),
+//     ],
+//   );
+// }
 
-class _ScrollUpButton extends StatefulWidget {
-  _ScrollUpButton({required this.controller});
+class MyData extends DataTableSource {
+  MyData({required this.visitors});
 
-  final ScrollController controller;
+  final List<Visitor> visitors;
 
-  @override
-  _ScrollUpButtonState createState() => _ScrollUpButtonState();
-}
-
-class _ScrollUpButtonState extends State<_ScrollUpButton> {
-  bool _showScrollUp = false;
-
-  @override
-  void initState() {
-    super.initState();
-    widget.controller.addListener(() {
-      if (widget.controller.position.pixels > 20 && !_showScrollUp) {
-        setState(() {
-          _showScrollUp = true;
-        });
-      } else if (widget.controller.position.pixels < 20 && _showScrollUp) {
-        setState(() {
-          _showScrollUp = false;
-        });
-      }
-      // On GitHub there was a question on how to determine the event
-      // of widget being scrolled to the bottom. Here's the sample
-      // if (widget.controller.position.hasViewportDimension &&
-      //     widget.controller.position.pixels >=
-      //         widget.controller.position.maxScrollExtent - 0.01) {
-      //   print('Scrolled to bottom');
-      //}
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return _showScrollUp
-        ? Positioned(
-            right: 10,
-            bottom: 10,
-            child: OutlinedButton(
-              child: Text('↑↑ go up ↑↑'),
-              onPressed: () => widget.controller.animateTo(0,
-                  duration: Duration(milliseconds: 300), curve: Curves.easeIn),
-              style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all(Colors.grey[800]),
-                  foregroundColor: MaterialStateProperty.all(Colors.white)),
-            ))
-        : SizedBox();
+  bool get isRowCountApproximate => false;
+  int get rowCount => visitors.length;
+  int get selectedRowCount => 0;
+  DataRow getRow(int index) {
+    return DataRow(cells: [
+      DataCell(Text(visitors[index].name)),
+      DataCell(Text(visitors[index].spell)),
+      DataCell(Text(visitors[index].ci.toString())),
+      DataCell(Text(visitors[index].solapin.toString())),
+      DataCell(Text(visitors[index].namePlace)),
+      DataCell(Text(visitors[index].nameWorker)),
+    ]);
   }
 }
